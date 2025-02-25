6c7aece9b978a04a73dcf02f12fa1f2f9cc8d3a5
stohirov <suxrobtoxirov001@gmail.com>
2025-02-25 13:12:47 +0500

    JwtUtil

--- src/main/java/stohirov/dev/task_application/TaskApplication.java
+++ src/main/java/stohirov/dev/task_application/TaskApplication.java
@@ -1,5 +1,6 @@
 package stohirov.dev.task_application;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TaskApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+
+		System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
+		System.setProperty("DB_USER", dotenv.get("DB_USER"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		System.setProperty("SERVER_PORT", dotenv.get("SERVER_PORT"));
+		System.setProperty("APP_NAME", dotenv.get("APP_NAME"));
+		System.setProperty("JWT_SECRET_KEY", dotenv.get("JWT_SECRET_KEY"));
+		System.setProperty("JWT_EXPIRY_TIME", dotenv.get("JWT_EXPIRY_TIME"));
 		SpringApplication.run(TaskApplication.class, args);
 	}
 
